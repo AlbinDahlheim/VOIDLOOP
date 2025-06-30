d84090028a788b31244059411add77be64ec9c9c
AlbinDahlheim <albin.dahlheim@gmail.com>
2025-06-30 19:49:50 +0200

    Player can run around + updated sprites slightly

--- Assets/Scripts/Player/PlayerBehavior.cs
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerBehavior : MonoBehaviour
 {
@@ -13,23 +14,33 @@ public class PlayerBehavior : MonoBehaviour
         LEFT,
         UP_LEFT
     }
-    public Direction FacingDirection => facingDirection;
-    private Direction facingDirection = Direction.DOWN; 
+    [HideInInspector] public Direction facingDirection = Direction.DOWN;
+
+    public float plusShapedDeadzone = 0.15f;
+    public float deadzone = 0.5f;
+
+    // Player Input Actions
+    public PlayerInputActions inputActions;
+    private InputAction moveAction;
 
     public Animator animator;
     public SpriteRenderer spriteRenderer;
     public Rigidbody2D rb2d;
     public AudioSource audioSource;
 
+    public Vector2 LeftStickInput => leftStickInput;
+    private Vector2 leftStickInput;
+
+    [HideInInspector] public Vector2 movementForce;
+    [HideInInspector] public Vector2 externalForce;
+
     public PlayerSheathed sheathedState = new PlayerSheathed();
     public PlayerUnsheathed unsheathedState = new PlayerUnsheathed();
     public PlayerStance stanceState = new PlayerStance();
 
-    public PlayerState CurrentState => currentState;
-    private PlayerState currentState;
-
     public PlayerState PreviousState => previousState;
     private PlayerState previousState;
+    private PlayerState currentState;
 
     public void OnValidate()
     {
@@ -38,14 +49,52 @@ public class PlayerBehavior : MonoBehaviour
         stanceState.OnValidate(this);
     }
 
-    private void Start()
+    private void Awake()
     {
-        
+        inputActions = new PlayerInputActions();
+        currentState = sheathedState;
+    }
+
+    private void OnEnable()
+    {
+        moveAction = inputActions.Player.Move;
+        moveAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
     }
 
     private void Update()
     {
-        
+        UpdateInput();
+
+        currentState.Update();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdatePhysics();
+
+        currentState.FixedUpdate();
+    }
+
+    private void UpdateInput()
+    {
+        leftStickInput = moveAction.ReadValue<Vector2>();
+        if (leftStickInput.magnitude <= deadzone)
+            leftStickInput = Vector2.zero;
+        if (Mathf.Abs(leftStickInput.x) <= plusShapedDeadzone)
+            leftStickInput.x = 0.0f;
+        if (Mathf.Abs(leftStickInput.y) <= plusShapedDeadzone)
+            leftStickInput.y = 0.0f;
+    }
+
+    private void UpdatePhysics()
+    {
+        rb2d.linearVelocity = movementForce + externalForce;
+        Debug.Log(rb2d.linearVelocity);
     }
 
     public void ChangeState(PlayerState targetState)
@@ -54,5 +103,26 @@ public class PlayerBehavior : MonoBehaviour
         previousState = currentState;
         currentState = targetState;
         currentState.Enter();
+    }
+
+    public Direction GetDirectionOfVector(Vector2 direction)
+    {
+        direction = direction.normalized;
+
+        int angle = Mathf.RoundToInt(Vector2.SignedAngle(Vector2.up, direction));
+        angle = angle <= 0 ? angle * -1 : 360 - angle;
+        int angleValue = (angle + 23) / 45 % 8;
+
+        return (PlayerBehavior.Direction)angleValue;
+    }
+
+    public string GetDirectionName()
+    {
+        string directionName = facingDirection.ToString();
+
+        directionName = directionName.Replace("RIGHT", "SIDE");
+        directionName = directionName.Replace("LEFT", "SIDE");
+
+        return directionName;
     }
 }
--- Assets/Scripts/Player/PlayerSheathed.cs
+++ Assets/Scripts/Player/PlayerSheathed.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerSheathed : PlayerState
 {
-    public float test;
+    public float movementSpeed;
 
     public override void OnValidate(PlayerBehavior player)
     {
@@ -14,5 +14,43 @@ public class PlayerSheathed : PlayerState
     public override void Enter()
     {
         // do stuff
+    }
+
+    public override void Update()
+    {
+        FlipDirection();
+
+        if (player.LeftStickInput != Vector2.zero)
+            UpdateRunning();
+        else
+            UpdateIdle();
+    }
+
+    private void FlipDirection()
+    {
+        if (player.facingDirection == PlayerBehavior.Direction.UP || player.facingDirection == PlayerBehavior.Direction.DOWN)
+            return;
+
+        if (player.LeftStickInput.x > 0.0f)
+            player.spriteRenderer.flipX = true;
+        else if (player.LeftStickInput.x < 0.0f)
+            player.spriteRenderer.flipX = false;
+    }
+
+    private void UpdateRunning()
+    {
+        player.facingDirection = player.GetDirectionOfVector(player.LeftStickInput);
+
+        Vector2 runVelocity = player.LeftStickInput.normalized * movementSpeed;
+        player.movementForce = runVelocity;
+
+        player.animator.Play($"RUN_{player.GetDirectionName()}_SHEATHED");
+    }
+
+    private void UpdateIdle()
+    {
+        player.movementForce = Vector2.zero;
+
+        player.animator.Play($"IDLE_{player.GetDirectionName()}_SHEATHED");
     }
 }
